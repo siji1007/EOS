2f00b21701e65587f7afc74e407ffaf4daaf15a3
BurgerFox1 <jeloaguilar1@gmail.com>
2025-06-01 03:59:36 +0800

    home page init
    di pa tapos will do other FE

--- Client/eos/src/pages/Home.tsx
+++ Client/eos/src/pages/Home.tsx
@@ -1,7 +1,14 @@
 const Home = () => {
     return(
         <>
-            <h1>This is the Home page</h1>
+            <div className="bg-linear-to-t from-[#213548] to-[#385981] w-full h-[90vh] flex flex-col items-baseline justify-center">
+                <h1 className="text-white text-9xl p-24 "><span className="text-[#1C80D5]">Simplify</span> Your Permit Process with <span className="text-[#1C80D5] font-bold">EOS</span></h1>
+                <div className="flex flex-row items-center justify-between w-full p-10">
+                    <button className="text-white font-extralight bg-transparent border-5 rounded-[10vh] text-5xl mx-50 h-[10vh] w-[20vw] transition hover:bg-[#385981]">Login</button>
+                    <img src="../src/assets/logo/logo.png" className="h-[30vh] aspect-auto"></img>
+                </div>
+                <h2 className="text-white text-3xl p-24"><span className="text-[#1C80D5] font-bold">Sign up </span>now or log in to manage your building applications — all in one secure and user-friendly platform.</h2>
+            </div>
         </>
 
     )
