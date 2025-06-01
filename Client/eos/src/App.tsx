e1703841f10580285681ef8ef5e7a7af60a3e63b
John Isaac Obusan <118874763+IsaacObusan@users.noreply.github.com>
2025-06-01 17:34:12 +0800

    Update App.tsx

--- Client/eos/src/App.tsx
+++ Client/eos/src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
-import LandingPage  from './pages/landingPage';
+import LandingPage from './pages/landingPage';
 import About from './pages/About';
 
 import Header from './partials/header';
@@ -11,7 +11,9 @@ import Footer from './partials/footer';
 import FormLayout from './admin/BuildingForm/FormLayout';
 import Assessment from './admin/Assessment';
 import Track from './admin/Track';
+import LoginModal from './Auth/login';
 import Settings from './admin/Settings';
+import Signup from './Auth/signup';
 
 function App() {
   return (
@@ -21,9 +23,13 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginModal isOpen={false} onClose={function (): void {
+              throw new Error('Function not implemented.');
+            } } />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/about" element={<About />} />
             <Route path="/admin" element={<LandingPage />}>
-              <Route index element={<Navigate to="Form" replace />} /> 
+              <Route index element={<Navigate to="Form" replace />} />
               <Route path="Form" element={<FormLayout />} />
               <Route path="Assessment" element={<Assessment />} />
               <Route path="Track" element={<Track />} />
