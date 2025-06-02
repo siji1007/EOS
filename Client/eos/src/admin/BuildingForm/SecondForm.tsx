47a25ae0be888c478f1b34345628df295eaf9ca4
John Isaac Obusan <118874763+IsaacObusan@users.noreply.github.com>
2025-06-02 10:30:21 +0800

    Update SecondForm.tsx

--- Client/eos/src/admin/BuildingForm/SecondForm.tsx
+++ Client/eos/src/admin/BuildingForm/SecondForm.tsx
@@ -4,75 +4,73 @@ const SecondForm = () => {
       <div className="grid grid-cols-3 gap-4">
         {/* Row 1 */}
         <section className="relative w-full mt-6 px-3">
-          <label  className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black" >Building</label>
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Building</label>
           <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
         </section>
 
-        <div>
-          <label className="block mb-1">Cost</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Cost</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
 
-        <div>
-          <label className="block mb-1">Number of storeys</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Number of storeys</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
 
         {/* Row 2 */}
-        <div>
-          <label className="block mb-1">Electrical</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
-        <div>
-          <label className="block mb-1">Equipment</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
-        <div>
-          <label className="block mb-1">Total Floor Area</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Electrical</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Equipment</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Total Floor Area</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
 
         {/* Row 3 */}
-        <div>
-          <label className="block mb-1">Mechanical</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
-        <div>
-          <label className="block mb-1">Installed</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
-        <div>
-          <label className="block mb-1">
-            Prop. Date of Const. Expected Date of Completion
-          </label>
-          <input type="date" className="w-full border rounded px-3 py-2" />
-        </div>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Mechanical</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Installed</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Prop. Date of Const. Expected Date of Completion</label>
+          <input type="date" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
 
         {/* Row 4 */}
-        <div>
-          <label className="block mb-1">Plumbing</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
-        <div>
-          <label className="block mb-1">Others</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
-        <div>
-          <label className="block mb-1">Material of const</label>
-          <select className="w-full border rounded px-3 py-2">
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Plumbing</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Others</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
+        <section className="relative w-full mt-6 px-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Material of const</label>
+          <select className="w-full border border-black rounded px-3 py-2 focus:outline-none">
             <option>Select</option>
             <option>Concrete</option>
             <option>Steel</option>
             <option>Wood</option>
             <option>Brick</option>
           </select>
-        </div>
+        </section>
 
         {/* Total Cost (full width) */}
-        <div className="col-span-3">
-          <label className="block mb-1">Total Cost</label>
-          <input type="text" className="w-full border rounded px-3 py-2" />
-        </div>
+        <section className="relative w-full mt-6 px-3 col-span-3">
+          <label className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black">Total Cost</label>
+          <input type="text" className="w-full border border-black rounded px-3 py-2 focus:outline-none" />
+        </section>
       </div>
     </div>
   );
