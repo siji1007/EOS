cad7a6e8f72117bbc57ed38119ad4e45d91c9806
John Isaac Obusan <118874763+IsaacObusan@users.noreply.github.com>
2025-06-02 10:27:37 +0800

    Update EigthForm.tsx

--- Client/eos/src/admin/BuildingForm/EigthForm.tsx
+++ Client/eos/src/admin/BuildingForm/EigthForm.tsx
@@ -4,22 +4,22 @@ const EighthForm = () => {
             
             <div className="gap-4">
                 {/* Row 1 */}
-                <div>
-                    <label className="block my-3 font-semibold">Community Tax Certificate</label>
-                    <input type="text" className="w-full border rounded px-3 py-2" />
-                </div>
+                <section className="relative w-full mt-6 px-3">
+                    <label htmlFor="communityTaxCertificate" className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black font-semibold">Community Tax Certificate</label>
+                    <input type="text" id="communityTaxCertificate" className="w-full border border-black rounded px-3 py-2 focus:outline-none" placeholder="Community Tax Certificate" title="Community Tax Certificate" />
+                </section>
 
                 {/* Row 2 */}
-                <div>
-                    <label className="block my-3 font-semibold">Date Issued</label>
-                    <input type="date" className="w-full border rounded px-3 py-2" />
-                </div>
+                <section className="relative w-full mt-6 px-3">
+                    <label htmlFor="dateIssued" className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black font-semibold">Date Issued</label>
+                    <input type="date" id="dateIssued" className="w-full border border-black rounded px-3 py-2 focus:outline-none" placeholder="Date Issued" title="Date Issued" />
+                </section>
 
                 {/* Row 3 */}
-                <div className="col-span-2">
-                    <label className="block my-3 font-semibold">Placed Issued</label>
-                    <input type="text" className="w-full border rounded px-3 py-2" />
-                </div>
+                <section className="relative w-full mt-6 px-3 col-span-2">
+                    <label htmlFor="placeIssued" className="absolute -top-3 ml-3 bg-gray-100 px-2 text-sm text-black font-semibold">Place Issued</label>
+                    <input type="text" id="placeIssued" className="w-full border border-black rounded px-3 py-2 focus:outline-none" placeholder="Place Issued" title="Place Issued" />
+                </section>
             </div>
         </div>
     );
